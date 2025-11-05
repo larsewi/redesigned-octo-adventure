25d2539ba67ea44e2dd1552796002b3cc6d12f04
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-11-05 17:05:37 +0100

    Move rendering to script
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

---
+++ src/render-changelog.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+#
+# render-changelog.sh: Render package changelog required by deb packaging tool
+# Args:
+#   $1 - Package string (e.g., 'curl')
+#   $2 - Package version (e.g., '8.16.0')
+#   $3 - Build date (e.g., 'Wed, 05 Nov 2025 16:38:12 +010')
+#
+
+PACKAGE_NAME="$1"
+PACKAGE_VERSION="$2"
+BUILD_DATE="$3"   
+
+mkdir "$PACKAGE_NAME-$PACKAGE_VERSION/debian"
+cat << EOF > "$PACKAGE_NAME"-"$PACKAGE_VERSION"/debian/changelog
+cfbuild-$PACKAGE_NAME ($PACKAGE_VERSION) unstable; urgency=low
+
+  * New build.
+
+ -- CFEngine Autobuild System <packager@cfengine.com>  $BUILD_DATE
+EOF
